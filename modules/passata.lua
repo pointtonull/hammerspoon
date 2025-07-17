e0128ba936d1b58d99ed39cbc9436a88091cd6eb
CC <carlos.cabrera@gcc.spire.com>
2025-07-17 03:15:10 +0100

    🚀 Add report peeking and stubs for future enhancements

--- modules/passata.lua
+++ modules/passata.lua
@@ -3,6 +3,25 @@ local M = {}
 
 local PASSATA = require("lib.passata")
 
+-- Peek the Passata report for a short duration and auto-hide it
+local function peek_report(alpha)
+    PASSATA:showReport(alpha)
+    hs.timer.doAfter(3, function() PASSATA.canvas_report:hide() end)
+end
+_G.peek_report = peek_report
+
+-- Stub for grayscale reminder (on_nagg trigger)
+local function gray_minute()
+    -- TODO: implement grayscale reminder
+end
+_G.gray_minute = gray_minute
+
+-- Stub for custom report drawing (on_draw_report trigger)
+local function customizeReport()
+    -- TODO: add custom elements to the report canvas
+end
+_G.customizeReport = customizeReport
+
 --- Initialize Passata timer and triggers
 function M.init()
     PASSATA:init()
